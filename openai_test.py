1174c99b9f1b9382209bdac5b1f9c43fd8401c1a
exdsgift <gabrieledurante01@gmail.com>
2024-02-20 16:02:00 +0100

    add a test file for GPT model and the jupiter for functions and structure of API

--- openai_test.py
+++ openai_test.py
@@ -1,12 +1,23 @@
 from openai import OpenAI
-client = OpenAI()
+from dotenv import load_dotenv
+import os
 
+# Carica le variabili d'ambiente dal file .env
+load_dotenv()
+
+# Recupera l'API key da una variabile d'ambiente
+api_key = os.getenv("OPENAI_API_KEY")
+
+# Assicurati che l'API key sia stata correttamente recuperata
+if api_key is None:
+    raise ValueError("OPENAI_API_KEY non è stato impostato nel file .env")
+
+# Utilizza l'API key per creare un'istanza del client OpenAI
+client = OpenAI(api_key=api_key)
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    {"role": "user", "content": 'What is the capital of France?'}
   ]
 )
-
 print(completion.choices[0].message)
