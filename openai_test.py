37b2d72e7d3fc3c7149ad3c4b192099effd52a8e
exdsgift <gabrieledurante01@gmail.com>
2024-02-20 19:29:52 +0100

    remove some italian transaltions

--- openai_test.py
+++ openai_test.py
@@ -2,17 +2,13 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
-# Carica le variabili d'ambiente dal file .env
 load_dotenv()
 
-# Recupera l'API key da una variabile d'ambiente
 api_key = os.getenv("OPENAI_API_KEY")
 
-# Assicurati che l'API key sia stata correttamente recuperata
 if api_key is None:
     raise ValueError("OPENAI_API_KEY non è stato impostato nel file .env")
 
-# Utilizza l'API key per creare un'istanza del client OpenAI
 client = OpenAI(api_key=api_key)
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
